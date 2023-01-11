9a6087a50f852321829f21cefe49b5efa7672b90
Phillip Krawetz <phillip.kraw@gmail.com>
2023-01-11 10:49:50 -0600

    add icon on system tray with 'Exit' option

--- PineconeScheduler.Client/App.axaml.cs
+++ PineconeScheduler.Client/App.axaml.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Windows.Input;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using PineconeScheduler.Client.ViewModels;
 using PineconeScheduler.Client.Views;
 
@@ -23,6 +28,32 @@ public partial class App : Application
       };
     }
 
+    InitializeTray();
+
     base.OnFrameworkInitializationCompleted();
+  }
+
+  private void InitializeTray()
+  {
+    var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+    var icons = new TrayIcons();
+    var icon = new TrayIcon();
+    icon.Menu = new NativeMenu();
+    icon.ToolTipText = "Pinecone";
+    icon.Icon = new WindowIcon(assets?.Open(
+        new Uri("avares://PineconeScheduler.Client/Assets/avalonia-logo.ico")));
+
+    var exitIcon = new NativeMenuItem();
+    exitIcon.Header = "Exit";
+    exitIcon.Click += ExitCommand;
+
+    icon.Menu.Items.Add(exitIcon);
+
+    icons.Add(icon);
+  }
+
+  public void ExitCommand(object? sender, object args)
+  {
+    (App.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.Shutdown();
   }
 }
